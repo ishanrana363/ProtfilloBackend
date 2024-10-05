0867b7f5c0bfb9f98f0ad9509c679d8012ab0691
ishanrana363 <ishanrana363@gmail.com>
2024-10-05 21:23:58 +0600

    app.js update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = new express();
 // Using rate limit middleware
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 })
 
 app.use(limiter)
